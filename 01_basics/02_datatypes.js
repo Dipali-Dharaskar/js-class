b98e87f5a67f571c8e376bebb5523336f0c48862
Dipali-Dharaskar <139240636+Dipali-Dharaskar@users.noreply.github.com>
2023-09-06 05:51:06 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3)  //we are using nodejs , not browser 
+
+
+
+console.log(3 + 3)
+
+console.log("dipali")
+
+
+let name = "dipali"
+let age = 26
+let isLoggedIn = false
+
+/*
+number => 2 to power 53
+bigInt
+string =>""
+boolean => true/false
+null =>stand alone value
+undefined =>
+symbol => unique
+
+object */
+
+console.log(typeof "dipali");
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
