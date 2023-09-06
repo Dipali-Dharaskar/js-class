29b785af50e3b4d57659318dc118f0ef10332ae6
Dipali-Dharaskar <139240636+Dipali-Dharaskar@users.noreply.github.com>
2023-09-06 05:23:17 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 125846;
+let accountEmail = "dipalidharskar@gmail.com"
+var accountPassword = "123456"
+accountCity = "akola"
+
+
+// accountId = 2; //not allowed
+
+accountEmail ="dd@gmail.com"
+accountPassword = "5252"
+accountCity = "pune"
+let accountState;
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+// console.log(accountId);
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+
